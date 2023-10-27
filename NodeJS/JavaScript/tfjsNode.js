056bfa4485e1a198908d0e63bcaf9441aec5ed07
Wjfjs <dreegon@naver.com>
2023-10-27 16:56:51 +0900

    test

--- NodeJS/JavaScript/tfjsNode.js
+++ NodeJS/JavaScript/tfjsNode.js
@@ -1,10 +1,17 @@
 const tf = require('@tensorflow/tfjs-node');
 const fs = require('fs');
-async function LoadModel(){
-  // 모델 로드
-  const model = await tf.loadLayersModel('file://Sinerva/ProjectSinerva/NodeJS/tfjs_model(final)/model.json');
-  console.log(tf.version);
+async function LoadModel() {
+    try {
+        const model = await tf.loadLayersModel('file://Sinerva/ProjectSinerva/NodeJS/tfjs_model(final)/model.json');
+        console.log(tf.version);
+        return model;
+    } 
+    catch (error) {
+        console.error('Error loading the model:', error);
+        throw error; // Optional: rethrow the error for further handling
+    }
 }
+
 module.exports = {
   LoadModel: LoadModel
 };
