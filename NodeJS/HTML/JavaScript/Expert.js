553da68222f15f1e87d8a57cb040417004644a18
SeongBeen <142138080+psb3505@users.noreply.github.com>
2023-10-29 14:24:09 +0900

    일반 유저 JS 파일 fetch post 경로 수정, 불러오기 select 기능 추가

--- NodeJS/HTML/JavaScript/Expert.js
+++ NodeJS/HTML/JavaScript/Expert.js
@@ -17,6 +17,32 @@ function InitPage(){
     PageLoad();
     
     prePage.style.visibility = "hidden";
+
+    // 해당 사용자 과거 기록 select 요청
+    let id = 'test';
+    return new  Promise((resolve, reject) => {
+        fetch('/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 데이터로 응답을 파싱
+        })
+        .then(data => {
+            resolve(data);
+            console.log(data); // 파싱된 JSON 데이터 출력
+            recordRow(data)
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
 }
 function SetPost(){
     var posts = document.getElementsByClassName("commentRequest");
@@ -73,3 +99,32 @@ function SetNextNum(){
 function SetCurrentPageText(currentPageNum){
     pageNum.textContent = currentPageNum;
 }
+
+function recordRow(data) {
+    // 테이블 요소를 가져옴
+    const table = document.getElementById("commentListTable");
+    let tableHTML = "";
+
+    tableHTML += "<tr id='commentListHeader'>";
+    tableHTML += "<th width='10%'>번호</th>";
+    tableHTML += "<th width='30%'>요청 날짜</th>";
+    tableHTML += "<th width='15%'>사진</th>";
+    tableHTML += "<th width='15%'>정상</th>";
+    tableHTML += "<th width='15%'>비정상</th>";
+    tableHTML += "<th width='15%'>상세보기</th>";
+    tableHTML += "</tr>";
+
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        tableHTML += "<tr id='commentListHeader'>";
+        tableHTML += `<td>${i + 1}</td>`;
+        for (const key in row) {
+            tableHTML += `<td>${row[key]}</td>`;
+        }
+        tableHTML += "<td><a href='#'>상세보기</a></td>";
+        tableHTML += "</tr>";
+    }
+
+    table.innerHTML = tableHTML;
+}
+
