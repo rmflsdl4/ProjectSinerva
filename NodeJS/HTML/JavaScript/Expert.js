196abd9a1ae3fe5301c9187432f91c25320ea561
Wjfjs <dreegon@naver.com>
2023-11-01 13:56:47 +0900

    test

--- NodeJS/HTML/JavaScript/Expert.js
+++ NodeJS/HTML/JavaScript/Expert.js
@@ -4,6 +4,7 @@ var prePage;
 var nextPage;
 var pageCount;
 var pageNum;
+var itemsPerPage = 5;
 
 function InitPage(){
     currPageNum = 1;
@@ -17,32 +18,6 @@ function InitPage(){
     PageLoad();
     
     prePage.style.visibility = "hidden";
-
-    // 해당 사용자 과거 기록 select 요청
-    let id = 'test';
-    return new  Promise((resolve, reject) => {
-        fetch('/record', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ id })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // JSON 데이터로 응답을 파싱
-        })
-        .then(data => {
-            resolve(data);
-            console.log(data); // 파싱된 JSON 데이터 출력
-            recordRow(data)
-        })
-        .catch(error => {
-            reject(error);
-        });
-    });
 }
 function SetPost(){
     var posts = document.getElementsByClassName("commentRequest");
@@ -100,6 +75,180 @@ function SetCurrentPageText(currentPageNum){
     pageNum.textContent = currentPageNum;
 }
 
+// ** 검사 **
+// 검사 결과 페이지 select 요청
+function InspectRecordInitPage(){
+    // 해당 사용자 과거 기록 select 요청
+    return new  Promise((resolve, reject) => {
+        fetch('/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 데이터로 응답을 파싱
+        })
+        .then(data => {
+            resolve(data);
+            console.log(data); // 파싱된 JSON 데이터 출력
+            InspectRecordRow(data)
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
+// 검사 결과 상세 페이지 select 요청
+function InspectDetailsRecordInitPage(date){
+    console.log(date);
+
+    // 해당 사용자 과거 기록 select 요청
+    return new  Promise((resolve, reject) => {
+        fetch('/detailsRecord', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ date })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 데이터로 응답을 파싱
+        })
+        .then(data => {
+            resolve(data);
+            console.log(data); // 파싱된 JSON 데이터 출력
+            InspectDetailsRecordRow(data)
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
+// 검사 결과 페이지 select 결과 출력
+function InspectRecordRow(data) {
+    // 테이블 요소를 가져옴
+    const table = document.getElementById("commentListTable");
+    let tableHTML = "";
+
+    tableHTML += "<tr id='commentListHeader'>";
+    tableHTML += "<th width='10%'>번호</th>";
+    tableHTML += "<th width='30%'>요청 날짜</th>";
+    tableHTML += "<th width='15%'>사진</th>";
+    tableHTML += "<th width='15%'>정상</th>";
+    tableHTML += "<th width='15%'>비정상</th>";
+    tableHTML += "<th width='15%'>상세보기</th>";
+    tableHTML += "</tr>";
+
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        tableHTML += "<tr class='commentRequest'>";
+        tableHTML += `<td><button class="checkBtn">${i + 1}</button></td>`;
+        for (const key in row) {
+            tableHTML += `<td>${row[key]}</td>`;
+        }
+        console.log(row.added);
+        tableHTML += `<td><a href="../InspectResultDetails.html?param1=${row.added}">상세보기</a></td>`;
+        tableHTML += "</tr>";
+    }
+
+    table.innerHTML = tableHTML;
+    InitPage();
+    PageLoad();
+}
+// 검사 결과 상세 페이지 select 결과 출력
+function InspectDetailsRecordRow(data) {
+    // 테이블 요소를 가져옴
+    const table = document.getElementById("commentListTable");
+    let tableHTML = "";
+
+    tableHTML += "<tr id='commentListHeader'>";
+    tableHTML += "<th width='10%'>번호</th>";
+    tableHTML += "<th width='30%'>요청 날짜</th>";
+    tableHTML += "<th width='20%'>사진</th>";
+    tableHTML += "<th width='10%'>상태</th>";
+    tableHTML += "<th width='30%'>코멘트</th>";
+    tableHTML += "</tr>";
+
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        tableHTML += "<tr class='commentRequest'>";
+        tableHTML += `<td>${i + 1}</td>`;
+        tableHTML += `<td>${row.added}</td>`;
+        tableHTML += `<td><img src="./images/${row.added}/${row.file_name}" style="width: 100%;"></td>`;
+        tableHTML += `<td>${row.result}</td>`;
+        tableHTML += "<td><button class='InspectBtn'>수락</button></td>";
+        tableHTML += "</tr>";
+    }
+
+    table.innerHTML = tableHTML;
+    InitPage();
+    PageLoad();
+}
+
+// ** 불러오기 **
+// 불러오기 페이지 select 요청
+function RecordInitPage(){
+    // 해당 사용자 과거 기록 select 요청
+    return new  Promise((resolve, reject) => {
+        fetch('/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 데이터로 응답을 파싱
+        })
+        .then(data => {
+            resolve(data);
+            console.log(data); // 파싱된 JSON 데이터 출력
+            recordRow(data)
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
+// 불러오기 상세 페이지 select 요청
+function DetailsRecordInitPage(date){
+    console.log(date);
+
+    // 해당 사용자 과거 기록 select 요청
+    return new  Promise((resolve, reject) => {
+        fetch('/detailsRecord', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ date })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // JSON 데이터로 응답을 파싱
+        })
+        .then(data => {
+            resolve(data);
+            console.log(data); // 파싱된 JSON 데이터 출력
+            DetailsRecordRow(data)
+        })
+        .catch(error => {
+            reject(error);
+        });
+    });
+}
+// 불러오기 페이지 select 결과 출력
 function recordRow(data) {
     // 테이블 요소를 가져옴
     const table = document.getElementById("commentListTable");
@@ -116,15 +265,45 @@ function recordRow(data) {
 
     for (let i = 0; i < data.length; i++) {
         const row = data[i];
-        tableHTML += "<tr id='commentListHeader'>";
-        tableHTML += `<td>${i + 1}</td>`;
+        tableHTML += "<tr class='commentRequest'>";
+        tableHTML += `<td><button class="checkBtn">${i + 1}</button></td>`;
         for (const key in row) {
             tableHTML += `<td>${row[key]}</td>`;
         }
-        tableHTML += "<td><a href='#'>상세보기</a></td>";
+        tableHTML += `<td><a href="../viewDetails.html?param1=${row.added}">상세보기</a></td>`;
         tableHTML += "</tr>";
     }
 
     table.innerHTML = tableHTML;
+    InitPage();
+    PageLoad();
 }
+// 불러오기 상세 페이지 select 결과 출력
+function DetailsRecordRow(data) {
+    // 테이블 요소를 가져옴
+    const table = document.getElementById("commentListTable");
+    let tableHTML = "";
 
+    tableHTML += "<tr id='commentListHeader'>";
+    tableHTML += "<th width='10%'>번호</th>";
+    tableHTML += "<th width='30%'>요청 날짜</th>";
+    tableHTML += "<th width='20%'>사진</th>";
+    tableHTML += "<th width='10%'>상태</th>";
+    tableHTML += "<th width='30%'>코멘트</th>";
+    tableHTML += "</tr>";
+
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        tableHTML += "<tr class='commentRequest'>";
+        tableHTML += `<td>${i + 1}</td>`;
+        tableHTML += `<td>${row.added}</td>`;
+        tableHTML += `<td><img src="./images/${row.added}/${row.file_name}" style="width: 100%;"></td>`;
+        tableHTML += `<td>${row.result}</td>`;
+        tableHTML += "<td>코멘트</td>";
+        tableHTML += "</tr>";
+    }
+
+    table.innerHTML = tableHTML;
+    InitPage();
+    PageLoad();
+}
