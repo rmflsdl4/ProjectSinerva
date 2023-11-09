48e8c594a68f5d5ca24baf0e9e9dcc811e3ce96f
SeongBeen <142138080+psb3505@users.noreply.github.com>
2023-11-09 19:49:53 +0900

    메인, 전문가 리스트 페이지 평점 수정

--- NodeJS/JavaScript/MainSys.js
+++ NodeJS/JavaScript/MainSys.js
@@ -1,7 +1,11 @@
 const database = require('../database.js');
 
 async function expertInfo() {
-	const query = `SELECT name, address, introduction FROM expert WHERE waitOk = 1`;
+	const query = `SELECT e.name as name, e.address as address, e.introduction as introduction, COALESCE(ROUND(AVG(ue.rating), 2), 0) AS rating
+					FROM expert AS e
+					LEFT JOIN user_has_expert AS ue ON e.id = ue.expert_id
+					WHERE e.waitOk = 1
+					GROUP BY e.id`;
 	
 	const result = await database.Query(query, null);
 	
